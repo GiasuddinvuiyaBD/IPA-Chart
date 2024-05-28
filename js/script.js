a18195f0913218973bd405593803f0d09dfebc56
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2024-05-28 22:09:27 +0600

    modified-html-and-js-file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const diphSound1 = document.querySelector('.diph-sound1');
+const diphSoundɪə = document.querySelector('.diph-sound-ɪə');
+const diphSoundeɪ = document.querySelector('.diph-sound-eɪ');
+const diphSoundʊə = document.querySelector('.diph-sound-ʊə');
+const diphSoundɔɪ = document.querySelector('.diph-sound-ɔɪ');
+const diphSoundəʊ = document.querySelector('.diph-sound-əʊ');
+const diphSoundeə = document.querySelector('.diph-sound-eə');
+const diphSoundaɪ = document.querySelector('.diph-sound-aɪ');
+const diphSoundaʊ = document.querySelector('.diph-sound-aʊ');
 
 
 // seeting audio 
@@ -14,11 +21,57 @@ const ɔɪ = new Audio("audio/ɔɪ.wav")
 const ʊə = new Audio("audio/ʊə.wav")
 
 
-
-diphSound1.addEventListener('click',(evt) => 
+diphSoundɪə.addEventListener('click',(evt) => 
 {
     evt.preventDefault(); 
     ɪə.play();
 })
 
+diphSoundeɪ.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    eɪ.play();
+})
+
+diphSoundʊə.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    ʊə.play();
+})
+
+diphSoundɔɪ.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    ɔɪ.play();
+})
+
+diphSoundəʊ.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    əʊ.play();
+})
+
+diphSoundeə.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    eə.play();
+})
+
+diphSoundɪə.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    ɪə.play();
+})
+
+diphSoundaɪ.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    aɪ.play();
+})
+
+diphSoundaʊ.addEventListener('click',(evt) => 
+{
+    evt.preventDefault(); 
+    aʊ.play();
+})
 
