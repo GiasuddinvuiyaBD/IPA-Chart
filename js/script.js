8913771c2cf1fba235f5d7805bd8d2cdf4442d5f
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2024-05-28 23:20:14 +0600

    changeses-the-class-name-of-consonant-and-monophthon-and-select-it-in-js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,46 @@ const diphSoundəʊ = document.querySelector('.diph-sound-əʊ');
 const diphSoundeə = document.querySelector('.diph-sound-eə');
 const diphSoundaɪ = document.querySelector('.diph-sound-aɪ');
 const diphSoundaʊ = document.querySelector('.diph-sound-aʊ');
+// monophthongs 
+const monophLongSoundsiː = document.querySelector(".monoph-long-sounds-iː");
+const monophLongSoundsuː = document.querySelector(".monoph-long-sounds-uː");
+const monophLongSoundsɜː = document.querySelector(".monoph-long-sounds-ɜː");
+const monophLongSoundsɔː = document.querySelector(".monoph-long-sounds-ɔː");
+const monophLongSoundsɑː = document.querySelector(".monoph-long-sounds-ɑː");
+
+const monophShortSoundsɪ = document.querySelector(".monoph-short-sounds-ɪ");
+const monophShortSoundsʊ = document.querySelector(".monoph-short-sounds-ʊ");
+const monophShortSoundse = document.querySelector(".monoph-short-sounds-e");
+const monophShortSoundsə = document.querySelector(".monoph-short-sounds-ə");
+const monophShortSoundsæ = document.querySelector(".monoph-short-sounds-æ");
+const monophShortSoundsʌ = document.querySelector(".monoph-short-sounds-ʌ");
+const monophShortSoundsɒ = document.querySelector(".monoph-short-sounds-ɒ");
+// consonant 
+const voicelessSoundsp = document.querySelector('.voiceless-sounds-p');
+const voicelessSoundst = document.querySelector('.voiceless-sounds-t');
+const voicelessSoundstʃ = document.querySelector('.voiceless-sounds-tʃ');
+const voicelessSoundsk = document.querySelector('.voiceless-sounds-k');
+const voicelessSoundsf = document.querySelector('.voiceless-sounds-f');
+const voicelessSoundsθ = document.querySelector('.voiceless-sounds-θ');
+const voicelessSoundss = document.querySelector('.voiceless-sounds-s');
+const voicelessSoundsʃ = document.querySelector('.voiceless-sounds-ʃ');
+
+const voicedSoundsb = document.querySelector('.voiced-sounds-b')
+const voicedSoundsd = document.querySelector('.voiced-sounds-d')
+const voicedSoundsdʒ = document.querySelector('.voiced-sounds-dʒ')
+const voicedSoundsg = document.querySelector('.voiced-sounds-g')
+const voicedSoundsv = document.querySelector('.voiced-sounds-v')
+const voicedSoundsð = document.querySelector('.voiced-sounds-ð')
+const voicedSoundsz = document.querySelector('.voiced-sounds-z')
+const voicedSoundsʒ = document.querySelector('.voiced-sounds-ʒ')
+const voicedSoundsm = document.querySelector('.voiced-sounds-m')
+const voicedSoundsn = document.querySelector('.voiced-sounds-n')
+const voicedSoundsŋ = document.querySelector('.voiced-sounds-ŋ')
+const voicedSoundsh = document.querySelector('.voiced-sounds-h')
+const voicedSoundsl = document.querySelector('.voiced-sounds-l')
+const voicedSoundsr = document.querySelector('.voiced-sounds-r')
+const voicedSoundsw = document.querySelector('.voiced-sounds-w')
+const voicedSoundsj = document.querySelector('.voiced-sounds-j')
 
 
 // seeting audio 
