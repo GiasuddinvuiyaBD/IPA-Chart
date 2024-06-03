b65ab194c9cac5c94da7ec3ac0fdcdb642260829
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2024-06-03 08:44:37 +0600

    added-consonant-sounds-and-aplyed-it

--- js/script.js
+++ js/script.js
@@ -1,55 +1,8 @@
-const diphSoundɪə = document.querySelector('.diph-sound-ɪə');
-const diphSoundeɪ = document.querySelector('.diph-sound-eɪ');
-const diphSoundʊə = document.querySelector('.diph-sound-ʊə');
-const diphSoundɔɪ = document.querySelector('.diph-sound-ɔɪ');
-const diphSoundəʊ = document.querySelector('.diph-sound-əʊ');
-const diphSoundeə = document.querySelector('.diph-sound-eə');
-const diphSoundaɪ = document.querySelector('.diph-sound-aɪ');
-const diphSoundaʊ = document.querySelector('.diph-sound-aʊ');
-// monophthongs 
-const monophLongSoundsiː = document.querySelector(".monoph-long-sounds-iː");
-const monophLongSoundsuː = document.querySelector(".monoph-long-sounds-uː");
-const monophLongSoundsɜː = document.querySelector(".monoph-long-sounds-ɜː");
-const monophLongSoundsɔː = document.querySelector(".monoph-long-sounds-ɔː");
-const monophLongSoundsɑː = document.querySelector(".monoph-long-sounds-ɑː");
-
-const monophShortSoundsɪ = document.querySelector(".monoph-short-sounds-ɪ");
-const monophShortSoundsʊ = document.querySelector(".monoph-short-sounds-ʊ");
-const monophShortSoundse = document.querySelector(".monoph-short-sounds-e");
-const monophShortSoundsə = document.querySelector(".monoph-short-sounds-ə");
-const monophShortSoundsæ = document.querySelector(".monoph-short-sounds-æ");
-const monophShortSoundsʌ = document.querySelector(".monoph-short-sounds-ʌ");
-const monophShortSoundsɒ = document.querySelector(".monoph-short-sounds-ɒ");
-// consonant 
-const voicelessSoundsp = document.querySelector('.voiceless-sounds-p');
-const voicelessSoundst = document.querySelector('.voiceless-sounds-t');
-const voicelessSoundstʃ = document.querySelector('.voiceless-sounds-tʃ');
-const voicelessSoundsk = document.querySelector('.voiceless-sounds-k');
-const voicelessSoundsf = document.querySelector('.voiceless-sounds-f');
-const voicelessSoundsθ = document.querySelector('.voiceless-sounds-θ');
-const voicelessSoundss = document.querySelector('.voiceless-sounds-s');
-const voicelessSoundsʃ = document.querySelector('.voiceless-sounds-ʃ');
-
-const voicedSoundsb = document.querySelector('.voiced-sounds-b')
-const voicedSoundsd = document.querySelector('.voiced-sounds-d')
-const voicedSoundsdʒ = document.querySelector('.voiced-sounds-dʒ')
-const voicedSoundsg = document.querySelector('.voiced-sounds-g')
-const voicedSoundsv = document.querySelector('.voiced-sounds-v')
-const voicedSoundsð = document.querySelector('.voiced-sounds-ð')
-const voicedSoundsz = document.querySelector('.voiced-sounds-z')
-const voicedSoundsʒ = document.querySelector('.voiced-sounds-ʒ')
-const voicedSoundsm = document.querySelector('.voiced-sounds-m')
-const voicedSoundsn = document.querySelector('.voiced-sounds-n')
-const voicedSoundsŋ = document.querySelector('.voiced-sounds-ŋ')
-const voicedSoundsh = document.querySelector('.voiced-sounds-h')
-const voicedSoundsl = document.querySelector('.voiced-sounds-l')
-const voicedSoundsr = document.querySelector('.voiced-sounds-r')
-const voicedSoundsw = document.querySelector('.voiced-sounds-w')
-const voicedSoundsj = document.querySelector('.voiced-sounds-j')
+// vowel sounds
+const pElm = document.querySelectorAll('.section-1 p');
 
 
-// seeting audio 
-const buttonAudio = new Audio("audio/final-button.wav");
+// sounds added here
 // const test = new Audio("audio/ɪə.mp3");
 const aɪ = new Audio("audio/aɪ.wav")
 const aʊ = new Audio("audio/aʊ.wav")
@@ -73,128 +26,181 @@ const ə = new Audio("audio/ə.wav")
 const æ = new Audio("audio/æ.wav")
 const ʌ = new Audio("audio/ʌ.wav")
 const ɒ = new Audio("audio/ɒ.wav")
+// consonant sounds
+const p = new Audio("audio/p.wav");
+const b = new Audio("audio/b.wav");
+const t = new Audio("audio/t.wav");
+const d = new Audio("audio/d.wav");
+const tʃ = new Audio("audio/tʃ.wav");
+const dʒ = new Audio("audio/dʒ.wav");
+const k = new Audio("audio/k.wav");
+const g = new Audio("audio/g.wav");
+const f = new Audio("audio/f.wav");
+const v = new Audio("audio/v.wav");
+const θ = new Audio("audio/θ.wav");
+const ð = new Audio("audio/ð.wav");
+const s = new Audio("audio/s.wav");
+const z = new Audio("audio/z.wav");
+const ʃ = new Audio("audio/ʃ.wav");
+const ʒ = new Audio("audio/ʒ.wav");
+const m = new Audio("audio/m.wav");
+const n = new Audio("audio/n.wav");
+const ŋ = new Audio("audio/ŋ.wav");
+const h = new Audio("audio/h.wav");
+const l = new Audio("audio/l.wav");
+const r = new Audio("audio/r.wav");
+const w = new Audio("audio/w.wav");
+const j = new Audio("audio/j.wav");
 
 
-
-
-diphSoundɪə.addEventListener('click',(evt) => 
+// 
+pElm.forEach((elm) =>
 {
-    evt.preventDefault(); 
-    ɪə.play();
-})
-
-diphSoundeɪ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    eɪ.play();
-})
-
-diphSoundʊə.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    ʊə.play();
-})
-
-diphSoundɔɪ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    ɔɪ.play();
-})
-
-diphSoundəʊ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    əʊ.play();
-})
-
-diphSoundeə.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    eə.play();
-})
-
-diphSoundɪə.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    ɪə.play();
-})
-
-diphSoundaɪ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    aɪ.play();
-})
- 
-diphSoundaʊ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault(); 
-    aʊ.play();
-})
-
-
-// sound vowel sounds
-monophLongSoundsiː.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    iː.play();
-})
-monophLongSoundsuː.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    uː.play();
-})
-monophLongSoundsɜː.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ɜː.play();
-})
-monophLongSoundsɔː.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ɔː.play();
-})
-monophLongSoundsɑː.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ɑː.play();
-})
-// short sounds 
-monophShortSoundsɪ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ɪ.play()
-})
-
-monophShortSoundsʊ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ʊ.play()
-})
-monophShortSoundse.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    e.play()
-})
-monophShortSoundsə.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ə.play()
-})
-
-monophShortSoundsæ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    æ.play()
-})
-monophShortSoundsʌ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ʌ.play()
-})
-monophShortSoundsɒ.addEventListener('click',(evt) => 
-{
-    evt.preventDefault();
-    ɒ.play()
+    
+    elm.addEventListener('click',(evt) => 
+    {
+        
+        evt.preventDefault()
+        console.log( evt.target.classList[0])
+        switch(true)
+        {
+            case evt.target.classList[0] === 'monoph-long-sounds-iː' : 
+              iː.play();
+            break;
+            case evt.target.classList[0] === 'monoph-long-sounds-uː' : 
+              uː.play();
+            break;
+            case evt.target.classList[0] === 'monoph-long-sounds-ɜː' : 
+              ɜː.play();
+            break;
+            case evt.target.classList[0] === 'monoph-long-sounds-ɔː' : 
+              ɔː.play();
+             break;
+            case evt.target.classList[0] === 'monoph-long-sounds-ɑː' : 
+              ɑː.play();
+            break;
+            // short sounds
+            case evt.target.classList[0] === 'monoph-short-sounds-ɪ' : 
+              ɪ.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-ʊ' : 
+              ʊ.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-e' : 
+              e.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-ə' : 
+              ə.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-æ' : 
+              æ.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-ʌ' : 
+              ʌ.play();
+            break;
+            case evt.target.classList[0] === 'monoph-short-sounds-ɒ' : 
+              ɒ.play();
+            break;
+            // dipththongs
+            case evt.target.classList[0] === 'diph-sound-ɪə' : 
+              ɪə.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-eɪ' : 
+              eɪ.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-ʊə' : 
+              ʊə.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-ɔɪ' : 
+              ɔɪ.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-əʊ' : 
+              əʊ.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-eə' : 
+              eə.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-aɪ' : 
+              aɪ.play();
+            break;
+            case evt.target.classList[0] === 'diph-sound-aʊ' : 
+              aʊ.play();
+            break;
+            // consonant sounds 
+            case evt.target.classList[0] === 'voiceless-sounds-p' : 
+              p.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-b' : 
+              b.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-t' : 
+              t.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-d' : 
+              d.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-tʃ' : 
+              tʃ.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-dʒ' : 
+            dʒ.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-k' : 
+             k.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-g' : 
+             g.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-f' : 
+             f.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-v' : 
+             v.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-θ' : 
+              θ.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-ð' : 
+              ð.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-s' : 
+              s.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-z' : 
+              z.play();
+            break;
+            case evt.target.classList[0] === 'voiceless-sounds-ʃ' : 
+              ʃ.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-ʒ' : 
+            ʒ.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-m' : 
+            m.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-n' : 
+            n.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-ŋ' : 
+            ŋ.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-h' : 
+            h.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-l' : 
+            l.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-r' : 
+            r.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-w' : 
+            w.play();
+            break;
+            case evt.target.classList[0] === 'voiced-sounds-j' : 
+            j.play();
+            break;
+        }
+    })
 })
 
 
